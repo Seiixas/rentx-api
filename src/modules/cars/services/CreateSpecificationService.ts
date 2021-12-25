25b039d33563ba5e24a9ce9abdba26e668162dec
Mateus Silva Seixas <mateusinhokarambi@hotmail.com>
2021-12-25 13:07:43 -0300

    Criando repositório de especificação

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,22 @@
+import { SpecificationRepository } from '../repositories/SpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute(): void {
-    // TODO
+  constructor(private specificationsRepository: SpecificationRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error('This specification already exists');
+    }
+
+    this.specificationsRepository.create({ name, description });
   }
 }
 
