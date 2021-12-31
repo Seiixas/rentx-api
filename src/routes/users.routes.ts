a77b59a359b9d428d26e22b49f81bc85449bfaa7
Mateus Seixas <mateusinhokarambi@hotmail.com>
2021-12-31 00:43:20 -0300

    Adicionando coluna de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express';
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post('/', createUserController.handle);
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes };
