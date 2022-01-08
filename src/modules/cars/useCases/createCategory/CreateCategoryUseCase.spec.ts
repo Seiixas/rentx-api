b07378cf3762c168c9627b8a3fcf9bdae7af89b1
Mateus Seixas <mateusinhokarambi@hotmail.com>
2022-01-08 09:53:23 -0300

    Refatorando a aplicação

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,4 +1,5 @@
-import { AppError } from '../../../../errors/AppError';
+import { AppError } from '@shared/errors/AppError';
+
 import { CategoresRepositorInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory';
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
