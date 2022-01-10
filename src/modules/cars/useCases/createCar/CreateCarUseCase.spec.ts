7fdd5d52e7fa2e79fbe567628a97b40771cb738f
Mateus Seixas <mateusinhokarambi@hotmail.com>
2022-01-10 10:44:27 -0300

    Continuando o caso de uso de carros

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,5 +1,6 @@
-import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+
+import { AppError } from '@shared/errors/AppError';
 
 import { CreateCarUseCase } from './CreateCarUseCase';
 
@@ -13,7 +14,7 @@ beforeEach(() => {
 
 describe('Create Car', () => {
   it('should be able to create a new car', async () => {
-    await createCarUseCase.execute({
+    const car = await createCarUseCase.execute({
       name: 'Car',
       description: 'Description car',
       daily_rate: 200,
@@ -22,5 +23,45 @@ describe('Create Car', () => {
       brand: 'Brand',
       category_id: 'Category',
     });
+
+    expect(car).toHaveProperty('id');
+  });
+
+  it('should not be able no create a new car if its license plate already exists', () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: 'Car 1',
+        description: 'Description car',
+        daily_rate: 200,
+        license_plate: 'ABC-1234',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'Category',
+      });
+
+      await createCarUseCase.execute({
+        name: 'Car 2',
+        description: 'Description car',
+        daily_rate: 200,
+        license_plate: 'ABC-1234',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'Category',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to create a new car with available true by default', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Car Availabe',
+      description: 'Description car',
+      daily_rate: 200,
+      license_plate: 'ABC-5555',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'Category',
+    });
+
+    expect(car.available).toBe(true);
   });
 });
