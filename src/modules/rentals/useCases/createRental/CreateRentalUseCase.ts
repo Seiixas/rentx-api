cdf106c483cb1046ed8c88e3131d89ac8a42192e
Mateus Seixas <mateusinhokarambi@hotmail.com>
2022-02-11 19:35:22 -0300

    Continuaçao Cadastro de Aluguel

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,3 +1,4 @@
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
 
 import { AppError } from '@shared/errors/AppError';
@@ -15,7 +16,7 @@ class CreateRentalUseCase {
     user_id,
     car_id,
     expected_return_date,
-  }: IRequest): Promise<void> {
+  }: IRequest): Promise<Rental> {
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
       car_id
     );
@@ -29,8 +30,16 @@ class CreateRentalUseCase {
     );
 
     if (rentalOpenToUser) {
-      throw new Error('There is a rental in progress for this user');
+      throw new AppError('There is a rental in progress for this user');
     }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
   }
 }
 
